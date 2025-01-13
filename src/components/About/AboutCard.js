30f62063fcb4cfd1e226e27b02d3acddf17f18a6
Chaitanya.K <132165121+Codexnever@users.noreply.github.com>
2025-01-13 17:09:02 +0530

    Update Job

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -10,7 +10,7 @@ function AboutCard() {
           Hey there! I'm <span className="purple">Chaitanya Kulthe </span>
             from <span className="purple"> Maharashtra, India.</span>
             <br />
-            Currently, I'm immersed in the world of Full-stack developer at MedResto.
+            Currently seeking a challenging role as a Full-stack Developer where I can contribute my skills and passion to a dynamic team.
             <br />
             <br />
             Besides coding, I'm passionate about exploring new technologies and enhancing my skills. I enjoy diving into projects that challenge me to think creatively and problem-solve effectively.
